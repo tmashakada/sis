1fec78c307ebd096ee6bb2446d8f2cff4c1a50ac
Tafadzwa Mashakada <tmashakada10@gmail.com>
2023-10-21 12:50:43 +0200

    initial commit

---
+++ sisbackend/src/main/java/com/lestech/sisbackend/SisbackendApplication.java
@@ -0,0 +1,13 @@
+package com.lestech.sisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisbackendApplication.class, args);
+	}
+
+}
---
+++ sisbackend/src/test/java/com/lestech/sisbackend/SisbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lestech.sisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
